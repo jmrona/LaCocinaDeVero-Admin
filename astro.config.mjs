6391499d0c44b2cfe497b8a656a721892bfcc0e0
Jose Romero Navarro <jose@forthwithlife.co.uk>
2025-04-27 20:46:57 +0100

    First commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel';
+import cleck from '@clerk/astro'
+
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  integrations: [cleck()],
+  output: "server",
+  adapter: vercel()
+});
---
+++ src/env.d.ts
@@ -0,0 +1,10 @@
+interface ImportMetaEnv {
+  readonly SUPABASE_URL: string
+  readonly SUPABASE_ANON_KEY: string
+  readonly PUBLIC_CLERK_PUBLISHABLE_KEY: string
+  readonly CLERK_SECRET_KEY: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
