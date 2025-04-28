1c4f5c30370995ebb70260eeb8a6ef95885efcbe
Jose Romero Navarro <jose@forthwithlife.co.uk>
2025-04-28 18:12:38 +0100

    Add shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,15 @@ import vercel from '@astrojs/vercel';
 import cleck from '@clerk/astro'
 
 
+import react from '@astrojs/react';
+
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  integrations: [cleck()],
+  integrations: [cleck(), react()],
   output: "server",
   adapter: vercel()
 });
