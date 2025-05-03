554245670b48e5a64681c0cf7228b5544b820a5b
Jose Romero Navarro <jose@forthwithlife.co.uk>
2025-05-03 13:51:02 +0100

    Add search params

--- src/lib/getDishes.ts
+++ src/lib/getDishes.ts
@@ -1,6 +1,6 @@
 import { supabase } from "./supabase";
 
-export const getDishes = async (page: number, perPage: number, sortBy?: string, order?: string) => {
+export const getDishes = async (page: number, perPage: number, sortBy?: string, order?: string, search?: string) => {
     const from = (page - 1) * perPage;
     const to = from + perPage - 1;
     let sortByColumn = "name->>es"
@@ -8,7 +8,7 @@ export const getDishes = async (page: number, perPage: number, sortBy?: string, 
     if(sortBy !== undefined) sortByColumn = sortBy
     if(sortBy === "name") sortByColumn = "name->>es"
 
-    const { data, error } = await supabase
+    const query = supabase
     .from('dishes')
     .select(`
         dish_id,
@@ -21,17 +21,42 @@ export const getDishes = async (page: number, perPage: number, sortBy?: string, 
           )
         ),
         categories:dishes_categories( category_id ),
+        categoriesV2:dishes_categories( category_id!inner(name, category_id) ),
         allergens:dishes_allergens( allergen_id )
-    `).order(sortByColumn, {ascending: order === "asc"}).range(from, to)
+    `)
 
+    if(search) query.ilike(`name->>es`, `%${search}%`)
+    
+    query.order(sortByColumn, {ascending: order === "asc"}).range(from, to)
+
+    const { data, error } = await query;
+    
     if (error) {
       console.error('Error fetching dishes:', error.message);
       return [];
     }
 
+    const { data: categoriesData, error: categoriesError } = await supabase
+    .from('categories')
+    .select(`*`)
+
+    if (categoriesError) {
+      console.error('Error fetching categories:', categoriesError.message);
+      return [];
+    }
+
+    const { data: allergensData, error: allergensError } = await supabase
+    .from('allergens')
+    .select(`*`)
+
+    if (allergensError) {
+      console.error('Error fetching allergens:', allergensError.message);
+      return [];
+    }
+
     return data.map(dish => {
-        const categories = dish.categories.map(cat => cat.category_id)
-        const allergens = dish.allergens.map(allergen => allergen.allergen_id)
+        const categories = dish.categories.map(cat => categoriesData?.find(category => category.category_id === cat.category_id).name.es)
+        const allergens = dish.allergens.map(all => allergensData?.find(allergen => allergen.allergen_id === all.allergen_id).name.es)
 
         return {
             id: dish.dish_id,
@@ -41,11 +66,5 @@ export const getDishes = async (page: number, perPage: number, sortBy?: string, 
             categories,
             allergens
         }
-    }).sort((a, b) => {
-      if (!sortBy) return 0
-      
-      // return a[sortBy].localeCompare(b[sortBy])
-      return 0
-    });
-
+    })
 }
